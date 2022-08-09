adc4c128c6d0c1059f4fc53cb53b6a365ae36425
robyrn <99139851+robyrn@users.noreply.github.com>
2022-08-09 17:11:59 -0500

    8/9 hotjar

---
+++ JS/index.js
@@ -0,0 +1,5 @@
+ScrollReveal({ duration: 1000})
+
+ScrollReveal().reveal(".heroImg", {delay: 500 });
+ScrollReveal().reveal(".MySkills", {delay: 500 });
+ScrollReveal().reveal(".myWork", {delay: 500 });
